e376c0a3ab525e0a4bf451290cbf2d22af8f1d57
Hugo Sant'Ana <hg.santana@outlook.com>
2023-11-05 10:28:08 -0300

    add environment

--- server/app.module.ts
+++ server/app.module.ts
@@ -4,11 +4,14 @@ import { join } from 'path'
 import AppServerModule from '../src/main.server'
 import { HealthController } from './controllers/health/health.controller'
 
+const BROWSER_FOLDER = process.env['PORT']
+  ? './browser'
+  : 'dist/a16n10ssr-template/browser'
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
       bootstrap: AppServerModule,
-      viewsPath: join(process.cwd(), 'dist/a16n10ssr-template/browser'),
+      viewsPath: join(process.cwd(), BROWSER_FOLDER),
     }),
   ],
   controllers: [HealthController],
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { HealthResponse } from 'server/models/health-response.model'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  readonly API_URL = 'http://localhost:4200'
-
   constructor(private http: HttpClient) {}
 
   checkHealth(): Observable<HealthResponse> {
-    return this.http.get<HealthResponse>(this.API_URL + '/api/health')
+    return this.http.get<HealthResponse>(environment.API_URL + '/api/health')
   }
 }
