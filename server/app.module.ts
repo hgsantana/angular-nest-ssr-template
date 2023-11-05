a0250e5c3a16b2fcc5316ae197888e9e323537fe
Hugo Sant'Ana <hg.santana@outlook.com>
2023-11-05 10:31:07 -0300

    fix port check for production

--- server/app.module.ts
+++ server/app.module.ts
@@ -4,9 +4,10 @@ import { join } from 'path'
 import AppServerModule from '../src/main.server'
 import { HealthController } from './controllers/health/health.controller'
 
-const BROWSER_FOLDER = process.env['PORT']
-  ? './browser'
-  : 'dist/a16n10ssr-template/browser'
+const BROWSER_FOLDER =
+  process.env['PORT'] === '8080'
+    ? './browser'
+    : 'dist/a16n10ssr-template/browser'
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
